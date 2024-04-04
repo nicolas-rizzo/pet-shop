b60a43677cfd95a8f63c162442e9718b1a224fce
Jair Cornejo <jaircornejo61@gmail.com>
2024-04-03 21:41:40 -0300

    hice la carga dinamica de productos con js

--- js/productos.js
+++ js/productos.js
@@ -19,6 +19,41 @@ const productos = [
         urlImagen:"https://d28hi93gr697ol.cloudfront.net/071e89ac-46a5-8ab3/img/Producto/0163acc7-daea-b9d4-6f76-b3918b2e4fd3/Alimento-Pro-Plan-Puppy-Raza-Mediana-18kg-63212d8a92d92.webp",
         descripcion:"Alimento para perro Pro Plan Puppy Razas Medianas 15 Kg + 3 Kg de Regalo",
         precio:15000
+    },
+    {
+        id:4,
+        animal:"Perro",
+        urlImagen:"https://d28hi93gr697ol.cloudfront.net/071e89ac-46a5-8ab3/img/Producto/0163acc7-daea-b9d4-6f76-b3918b2e4fd3/Alimento-Pro-Plan-Puppy-Raza-Mediana-18kg-63212d8a92d92.webp",
+        descripcion:"Alimento para perro Pro Plan Puppy Razas Medianas 15 Kg + 3 Kg de Regalo",
+        precio:15000
+    },
+    {
+        id:5,
+        animal:"Perro",
+        urlImagen:"https://d28hi93gr697ol.cloudfront.net/071e89ac-46a5-8ab3/img/Producto/0163acc7-daea-b9d4-6f76-b3918b2e4fd3/Alimento-Pro-Plan-Puppy-Raza-Mediana-18kg-63212d8a92d92.webp",
+        descripcion:"Alimento para perro Pro Plan Puppy Razas Medianas 15 Kg + 3 Kg de Regalo",
+        precio:15000
+    },
+    {
+        id:6,
+        animal:"Perro",
+        urlImagen:"https://d28hi93gr697ol.cloudfront.net/071e89ac-46a5-8ab3/img/Producto/0163acc7-daea-b9d4-6f76-b3918b2e4fd3/Alimento-Pro-Plan-Puppy-Raza-Mediana-18kg-63212d8a92d92.webp",
+        descripcion:"Alimento para perro Pro Plan Puppy Razas Medianas 15 Kg + 3 Kg de Regalo",
+        precio:15000
+    },
+    {
+        id:7,
+        animal:"Perro",
+        urlImagen:"https://d28hi93gr697ol.cloudfront.net/071e89ac-46a5-8ab3/img/Producto/0163acc7-daea-b9d4-6f76-b3918b2e4fd3/Alimento-Pro-Plan-Puppy-Raza-Mediana-18kg-63212d8a92d92.webp",
+        descripcion:"Alimento para perro Pro Plan Puppy Razas Medianas 15 Kg + 3 Kg de Regalo",
+        precio:15000
+    },
+    {
+        id:8,
+        animal:"Perro",
+        urlImagen:"https://d28hi93gr697ol.cloudfront.net/071e89ac-46a5-8ab3/img/Producto/0163acc7-daea-b9d4-6f76-b3918b2e4fd3/Alimento-Pro-Plan-Puppy-Raza-Mediana-18kg-63212d8a92d92.webp",
+        descripcion:"Alimento para perro Pro Plan Puppy Razas Medianas 15 Kg + 3 Kg de Regalo",
+        precio:15000
     }
 ];
 
@@ -38,5 +73,33 @@ const cargarListaAnimales =()=>{
     })
 }
 
-
 cargarListaAnimales();
+
+const cargarProductos =()=>{
+    const listaProductos = document.getElementById('listaProductos');
+    productos.forEach(producto => {
+        let divProducto = document.createElement('div')
+        let imagenProducto = document.createElement('img');
+        let descripcionProducto = document.createElement('p');
+        let precioProductos = document.createElement('p');
+
+        divProducto.className="contenedorProducto"
+
+        imagenProducto.src=producto.urlImagen;
+        imagenProducto.loading="lazy";
+        imagenProducto.className="imagenProducto"
+
+        descripcionProducto.textContent=producto.descripcion;
+        descripcionProducto.className="descripcionProducto"
+
+        precioProductos.textContent=`${producto.precio}$`
+        precioProductos.className="precioProducto"
+
+        divProducto.appendChild(imagenProducto);
+        divProducto.appendChild(descripcionProducto);
+        divProducto.appendChild(precioProductos);
+        listaProductos.appendChild(divProducto)
+    })
+}
+
+cargarProductos();
