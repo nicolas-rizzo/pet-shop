12ca56b14186edff0b3ebda5ac23f5ed2a7110bb
Nicolas Rizzo <nicolas.rizzo@gmail.com>
2024-06-17 12:32:53 -0300

    Scripts database

---
+++ scripts/ScriptPetShop.sql
@@ -0,0 +1,51 @@
+DROP DATABASE IF EXISTS petShop;
+
+create database petShop;
+
+use petShop;
+
+create table animales(
+	idAnimal_AN int auto_increment not null,
+	nombre_AN varchar(20) not null,
+	primary key (idAnimal_AN)
+);
+
+create table productos(
+	idProducto_PR int auto_increment not null,
+	idAnimal_PR int not null,
+	urlImagen_PR varchar(300) not null,
+	descripcion_PR varchar(110) not null,
+	precioUnitario_PR decimal(8,2) not null,
+	activo_PR boolean not null default 1,
+	primary key (idProducto_PR),
+	foreign key (idAnimal_PR) references animales(idAnimal_AN)
+);
+
+create table usuarios(
+	idUsuario_US int auto_increment not null,
+	nombreUsuario_US varchar(40) not null,
+	correoElectronico_US char(40) not null unique,
+	contraseña_US char(30) not null,
+	domicilio_US varchar(40) not null,
+	codigoPostal varchar(10) not null,
+	primary key (idUsuario_US)
+);
+
+create table ventas(
+	idVenta_VN int auto_increment not null,
+	idUsuario_VN int not null,
+	importeTotal_VN decimal(8,2) not null default 0,
+	fechaVenta_VN date not null default (CURRENT_DATE),
+	primary key (idVenta_VN),
+	foreign key (idUsuario_VN) references usuarios(idUsuario_US)
+); 
+
+create table detalleVenta(
+	idVenta_DV int not null,
+	idProducto_DV int not null,
+	cantidad_DV int not null,
+	precioUnitario decimal(8,2) not null,
+	primary key(idVenta_DV,idProducto_DV),
+	foreign key(idVenta_DV) references ventas(idVenta_VN),
+	foreign key(idProducto_DV) references productos(idProducto_PR)
+);
---
+++ scripts/insertPetShop.sql
@@ -0,0 +1,25 @@
+use petShop;
+
+insert into animales (nombre_AN) values
+	('Perro'),('Gato'),('Aves'),('Conejo'),('Tortuga');	
+	
+select * from animales;
+
+insert into productos (idAnimal_PR,urlImagen_PR,descripcion_PR,precioUnitario_PR)
+	values (1,
+	'https://d28hi93gr697ol.cloudfront.net/071e89ac-46a5-8ab3/img/Producto/0163acc7-daea-b9d4-6f76-b3918b2e4fd3/Alimento-Pro-Plan-Puppy-Raza-Mediana-18kg-63212d8a92d92.webp',
+	'Alimento para perro Pro Plan Puppy Razas Medianas 15 Kg + 3 Kg de Regalo',
+	15000),
+	(2,'https://www.paraisodemascotas.com.ar/wp-content/uploads/2019/05/gati-carno-paraiso-de-mascotas-parana-super-mascota.webp',
+	'Gati Adultos sabor carne y pollo a la jardinera y otras proteinas',20000),
+	(3,'https://catycanar.vtexassets.com/arquivos/ids/156453/10671.jpg?v=637732970377270000',
+	'Alimento Para Canarios Tropmix Zootec X 1 Kg',10000),
+	(4,'https://acdn.mitiendanube.com/stores/001/791/617/products/15-342d7c0d398dc3023717063887056578-640-0.png',
+	'SCART Producto Alimento para conejos | Champion Mini Pets',9000),
+	(5,'https://i5.walmartimages.com.mx/mg/gm/3pp/asr/dcdb2ebe-d6ae-4bcd-8ee3-711ce7799406.aff2f83251b55cd062271dc76495b859.png?odnHeight=612&odnWidth=612&odnBg=FFFFFF',
+	'Alimento para Tortugas Acuaticas Lomas Reptile Sticks Palitos 300 gr',
+	15000);
+	
+
+select * from productos;
+
