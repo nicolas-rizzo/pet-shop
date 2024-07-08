76964426b2405f846ff5ffd9d9b74fac5dcc244f
Nicolas Rizzo <nicolas.rizzo@gmail.com>
2024-07-08 16:19:07 -0300

    Cambios en clear cookie al cerrar sesion

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -76,7 +76,15 @@ export default class UsuarioController {
     }
 
     logout = async (req, res) => {
-        res.clearCookie('access_token')
+        //res.clearCookie('access_token')
+
+        res.clearCookie('access_token', {
+            domain: 'pet-shop-ten-ruddy.vercel.app', 
+            secure: true, 
+            httpOnly: true, 
+            sameSite: 'None' 
+        })
+
         res.status(200).json({ mensaje: 'Sesion cerrada.' })
     }
 
